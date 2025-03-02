86963143df0b53160faade5eaddc1a60410cf329
saiznistaken <acelik2022@hotmail.com>
2025-03-02 19:45:06 +0300

    prime

---
+++ primeNumber.py
@@ -0,0 +1,13 @@
+def isPrime(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+    
+num = int(input("Enter a number: "))
+if isPrime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
