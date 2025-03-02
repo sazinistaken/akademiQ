205c9da48cfbbab63c73474d0ea3696abbfd6aed
saiznistaken <acelik2022@hotmail.com>
2025-03-02 20:34:34 +0300

    finished

--- calculator.py
+++ calculator.py
@@ -8,16 +8,21 @@ def calculator(num1, num2, operator):
             return num1 * num2
         case 4:
             if num2 == 0:
-                print("Denominator cannot be \"0\".")
+                return "Denominator cannot be \"0\"."
             else:
                 return num1 / num2 
-        case _:
-            return "Invalid chocice"
         
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2: "))
 
-print("1. Addition (+)\n2. Substraction (-)\n3. Multiplication (*)\n4. Division (/)")
-operator = int(input("Enter your choice: \n"))
-
-print(calculator(num1, num2, operator))
+while True:
+    print("1. Addition (+)\n2. Substraction (-)\n3. Multiplication (*)\n4. Division (/)\n5. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 5:
+        print("Exiting..")
+        break
+    elif choice > 5 or choice < 1:
+        print("Invalid choice.\n")
+        print("***********************\n")
+        continue
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    print(f"{calculator(num1, num2, choice)}\n")
